bb5b03197401a263c13a72ba9dea965bde60aaac
BrianMcL <bmclaugh28@yahoo.com>
2020-02-04 23:27:23 -0500

    moved style and script into the html files, added ADD and DELETE notes

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ var app = express();
 var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use (express.static(path.join(__dirname, 'public'))); 
+
+// **********************************************
+// globals
+// **********************************************
+var maxID = 0; 
 
 // **********************************************
 // routes 
@@ -28,7 +34,7 @@ app.get("/notes", function(req, res) {
 // get the latest db
 app.get("/api/notes", function(req, res) {
 
-   console.log ("here"); 
+   //console.log ("here"); 
    fs.readFile ("./assets/db/db.json", function (err, data){
       if (err){
          console.log (err); 
@@ -40,6 +46,67 @@ app.get("/api/notes", function(req, res) {
    });
 });
 
+app.delete ("/api/notes/:id", function (req, res){
+
+   console.log ('delete ' + req.params.id); 
+   fs.readFile ("./assets/db/db.json", function (err, data){
+      if (err) throw (err); 
+      let notes = JSON.parse(data); 
+      console.log(notes);
+
+      for (let i=0;i<notes.length;i++){
+         if (notes[i].id==req.params.id){
+
+            notes.splice(i,1);  
+            console.log (" "); 
+            console.log (notes); 
+
+            let noteStr = JSON.stringify(notes);
+            fs.writeFile ("./assets/db/db.json", noteStr, (err)=>{
+               if (err) throw err; 
+               return res.json (true); 
+            }); 
+         }
+      }
+   }); 
+});
+
+app.post("/api/notes", function(req, res) {
+
+   fs.readFile ("./assets/db/db.json", function (err, data){
+      if (err) throw (err); 
+      let notes = JSON.parse(data); 
+
+      // this is clunky, but we'll use FOR to get the maxID. Remember, the 
+      // function returns the max ID, not the NEXT ID, so increment!
+      let nextID = getNextID(notes) + 1;  
+
+      let newNote = {
+         id: nextID, 
+         title: req.body.title, 
+         text: req.body.text
+      }
+      notes.push(newNote); 
+
+      let noteStr = JSON.stringify(notes);
+      fs.writeFile ("./assets/db/db.json", noteStr, (err)=>{
+         if (err) throw err; 
+         return res.json (newNote); 
+      }); 
+   });
+}); 
+
+// **********************************************
+// functions 
+// **********************************************
+
+function getNextID (notes){
+   var maxID = 0; 
+   for (let i=0;i<notes.length;i++){
+      maxID = Math.max(notes[i].id, maxID); 
+   }
+   return maxID; 
+}
 
 // **********************************************
 // listen 
